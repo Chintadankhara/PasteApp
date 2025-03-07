cb44f8fa4a72021f080d05fe136791c7342ab141
xuser <chintandankhara9@gmail.com>
2025-03-07 14:43:54 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome friends !</h1>  <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat atque maxime, fugiat dolorum suscipit ipsam impedit mollitia quisquam, enim officiis distinctio dolor expedita omnis maiores iure iusto facilis obcaecati? Voluptatibus at, eaque dolorum magnam delectus dolor nobis dolore sed, repellendus totam earum iste, veritatis error voluptates ratione. Fuga consequatur corrupti fugit reiciendis dicta, molestiae aspernatur quidem explicabo excepturi nobis magnam totam quasi non odit vero quod modi cum dolorem ex culpa fugiat. Excepturi, quos veniam itaque debitis corporis voluptate velit voluptas exercitationem asperiores non doloremque qui eius rerum perspiciatis in labore a sequi illum repellendus quasi, ratione fuga consectetur? At ab alias temporibus, provident quia saepe laboriosam. Nulla quas saepe eveniet voluptates, officiis optio iure reiciendis error pariatur doloribus fuga perferendis, architecto iste in blanditiis, nesciunt aut impedit eum amet. Odit laboriosam ut beatae, velit ea quia delectus voluptatibus natus eum animi ab, quas necessitatibus reiciendis iste. Eligendi, distinctio, harum maiores dolor fuga aut cum quisquam incidunt provident sed magni pariatur, saepe reprehenderit dolores! Laborum odio molestiae, exercitationem molestias architecto veritatis a voluptate ipsa est cum deleniti ipsam vero, sint ad quod quia ducimus eligendi. Officiis consequuntur, magnam rerum nulla inventore nam sapiente sed tempore, nesciunt id, illum quam! Praesentium!</p>
+      <h2>Im  student from gec</h2> <br />
+      <h2>Who are seeking for good job</h2>
+      
     </>
   )
 }
